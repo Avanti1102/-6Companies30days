ee8a0ffd6f41ae60086977f001ce12d92d22aa1b
Avanti <avantibanginwar2002@gmail.com>
2024-12-31 23:56:29 +0530

    FindTheWinnerOfTheCircularGame added

---
+++ Microsoft/FindTheWinnerOfTheCircularGame.java
@@ -0,0 +1,10 @@
+class Winner {
+    public int findTheWinner(int n, int k) {
+        int result=0;
+        for(int i=1;i<=n;i++){
+            result = (result+k) % i;
+        }
+        return result+1;
+
+    }
+}
